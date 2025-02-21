f9cb49725195084aa28684247260fe366960eed8
keonaabad <keonaabad2004@gmail.com>
2025-02-20 16:35:51 -0800

    adjusting leaderboard calling

--- gamedev/controllers/leaderboardController.js
+++ gamedev/controllers/leaderboardController.js
@@ -3,9 +3,9 @@ import pool from '../../database/connection_pool.mjs';
 export const getLeaderboard = async (req, res) => {
     try {
         const result = await pool.query(
-            `SELECT users.username, SUM(distance) as total_distance
-             FROM runs
-             JOIN users ON users.user_id = runs.user_id
+            `SELECT users.username, COALESCE(SUM(runs.distance), 0) as total_distance
+             FROM users
+             LEFT JOIN runs ON users.user_id = runs.user_id
              GROUP BY users.username
              ORDER BY total_distance DESC
              LIMIT 10`
