e9dd4f17f21faf7e61ea573a6e235de13c8780d6
btmunger <btmunger@gmail.com>
2025-02-27 15:17:44 -0800

    Fixed error handling / redirection error

--- gamedev/public_html/createaccount.js
+++ gamedev/public_html/createaccount.js
@@ -1,9 +1,9 @@
 // Wait until the page is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
     // When page loads, hide all errors
-    const passwordError = document.getElementById('password_error');
+    const passwordError = document.getElementById('password-error');
     passwordError.style.visible = "hidden";
-    const accountError = document.getElementById('account_error');
+    const accountError = document.getElementById('account-error');
     accountError.style.visible = "hidden";
     // Wait until the user submits the form
     document.querySelector("form").addEventListener("submit", async function (event) {
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const password = document.getElementById("password").value;
         const confirm_password = document.getElementById("confirm_password").value;
         // API URL for the backend
-        const API_URL = 'https://run-for-your-life-api.onrender.com';
+        const API_URL = 'https://run-for-your-life-api.onrender.com/';
 
         // If the passwords match, send a POST request to the API
         if (password === confirm_password) {
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 const response = await fetch(`${API_URL}/api/users/register`, {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
+                    // Send the username, email, passowr
                     body: JSON.stringify({ username, email, password })
                 });
     
@@ -32,10 +33,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (response.ok) {
                     console.log("User created!");
                     localStorage.setItem('user_id', data.user_id);
-                    window.location.href = "map.html";
+                    window.location.href = "https://run-for-your-life.onrender.com/";
                 // If the register account did not work, display error message to the user
                 } else {
-                    errorMessage.style.display = "block";
+                    accountError.style.display = "visible";
                     console.log("Error creating user");
                 }
             // Display any other errors to the console
